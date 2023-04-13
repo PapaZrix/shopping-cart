4b78386ac8cf709456f5b05eeedd8b67e2b5b024
PapaZrix <zrilic313@gmail.com>
2023-04-13 20:10:56 +0200

    Add items to database

---
+++ src/db/db.js
@@ -0,0 +1,60 @@
+const db = [
+    {
+        name: 'Fight Milk T-Shirt',
+        id: 'fight-milk',
+        endpoint: 'MILK-100011-Adult-ShortSleeve-Tshirt-Black-MF_720x.png?v=1636655793',
+        qty: 1,
+        price: 24.99
+    },
+    {
+        name: 'Trash Man T-Shirt',
+        id: 'trash-man',
+        endpoint: 'TRASH-100011-GRY-MF_720x.png?v=1643141596',
+        qty: 1,
+        price: 24.99
+    },
+    {
+        name: 'Jabroni Sweatshirt',
+        id: 'jabroni-sweat',
+        endpoint: 'JAB-100000-Crewneck-Sweatshirt-MF_720x.png?v=1636655822',
+        qty: 1,
+        price: 29.99
+    },
+    {
+        name: 'Ongo Gablogian Hat',
+        id: 'ongo-hat',
+        endpoint: 'ONGO-100174-Hat-MF_720x.png?v=1636654683',
+        qty: 1,
+        price: 19.99
+    },
+    {
+        name: 'Fight Milk Hat',
+        id: 'fight-milk-hat',
+        endpoint: 'MILK-100174-Hat-Black-MF_720x.png?v=1636655808',
+        qty: 1,
+        price: 19.99
+    },
+    {
+        name: 'Icons Beanie',
+        id: 'icons-beanie',
+        endpoint: 'ICONS-100175-MF_720x.png?v=1630517887',
+        qty: 1,
+        price: 19.99
+    },
+    {
+        name: 'The Nightman Cometh Mug',
+        id: 'nightman-mug',
+        endpoint: 'TNC-11oz-black-mug-left_720x.png?v=1636657036',
+        qty: 1,
+        price: 14.99
+    },
+    {
+        name: 'Dennis System Poster',
+        id: 'dennis-system',
+        endpoint: 'DS-100123-24x36-White-MF_720x.png?v=1630517858',
+        qty: 1,
+        price: 38.99
+    }
+]
+
+export default db
